a012f90f4667b103ce000434e4f55bf98be1423e
tdat <datnguyen4804@gmail.com>
2025-10-27 12:17:22 +0700

    heee

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const { setupSequelize, connectDB } = require("./Config/database.js");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/user-routes.js"));
-app.use("/api/exam", require("./routes/exam-routes.js"))
+app.use("/api/exam", require("./routes/exam-routes.js"));
 app.use("/api/classManagement", require("./routes/classManagement-routes.js"));
 app.use("/api/teacher", require("./routes/teacher-class-routes.js"));
 app.use("/api/student", require("./routes/student-classs-routes.js"));
 
-// const redoc = require('redoc-express');
-// app.get('/docs/api-documents.json', (req, res) => {
-//   const filePath = path.join(__dirname, 'docs', 'api-documents.json');
-//   if (!fs.existsSync(filePath)) {
-//     return res.status(404).json({ error: 'File not found' });
-//   }
-//   res.setHeader('Content-Type', 'application/json');
-//   res.sendFile(filePath);
-// });
-app.use('/docs', express.static(path.join(__dirname, 'docs')));
+app.use("/docs", express.static(path.join(__dirname, "docs")));
 
+connectDB()
+  .then(() => {
+    setupSequelize.sync({ alter: true })
+      .then(() => console.log("Database đã được đồng bộ"))
+      .catch((err) => console.error("Lỗi đồng bộ database:", err));
+  })
+  .catch((err) => console.error("Lỗi kết nối database:", err));
 
-connectDB().then(() => {
-  setupSequelize.sync({ alter: true }).then(() => {
-    console.log("Database đã được đồng bộ");
-  }).catch((err) => {
-    console.error("Lỗi đồng bộ database:", err);
-  });
-  const portServer = process.env.PORT || 3050
-  app.listen(portServer, () => {
-    console.log(`Server đang chạy tại http://localhost:${portServer}`);
-  });
-});
-
+// ✅ Export app cho Vercel
+module.exports = app;
