3ddebb922bd8e539dd8ab83819bdf003eb127207
tdat <datnguyen4804@gmail.com>
2025-10-31 16:11:47 +0700

    gggg

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get("/openapi.json", (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "docs")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "redoc.html"));
